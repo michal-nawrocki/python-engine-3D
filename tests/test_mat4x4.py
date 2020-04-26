ca951ec3fc4bc26828c5ee47072facb95209d7f4
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2020-04-26 02:05:27 +0100

    Feature-4: Additional Mat4x4 functions and their UTs

--- tests/test_mat4x4.py
+++ tests/test_mat4x4.py
@@ -9,8 +9,8 @@ class TestMat4x4(unittest.TestCase):
     """
     Unit test of Mat4x4
     """
-    def test_vector_multiplication(self):
-        """ Testing * operator """
+    def test_vector_multiplication_with_vec3(self):
+        """ Testing * operator with Vec3 """
         vec = Vec3(1, 2, 3)
         matrix = Mat4x4()
 
@@ -22,6 +22,27 @@ class TestMat4x4(unittest.TestCase):
         self.assertEqual(10, result.x, "Asserting vec.x from matrix-vector multiplication")
         self.assertEqual(9, result.y, "Asserting vec.y from matrix-vector multiplication")
         self.assertEqual(7, result.z, "Asserting vec.z from matrix-vector multiplication")
+
+    def test_vector_multiplication_with_mat4x4(self):
+        """ Testing * operator with Mat4x4"""
+        matrix_1 = Mat4x4.identity_matrix()
+        matrix_2 = Mat4x4()
+
+        matrix_2.m[0] = [4, 3, 1, 0]
+        matrix_2.m[2] = [2, 2, 2, 0]
+
+        result = matrix_1 * matrix_2
+        result = result.m
+
+        row_0 = [4.0, 0.0, 2.0, 0.0]
+        row_1 = [3.0, 0.0, 2.0, 0.0]
+        row_2 = [1.0, 0.0, 2.0, 0.0]
+        row_3 = [0.0, 0.0, 0.0, 0.0]
+
+        self.assertEqual(row_0, result[0], "Asserting mat[0] row for matrix-matrix multiplication")
+        self.assertEqual(row_1, result[1], "Asserting mat[1] row for matrix-matrix multiplication")
+        self.assertEqual(row_2, result[2], "Asserting mat[2] row for matrix-matrix multiplication")
+        self.assertEqual(row_3, result[3], "Asserting mat[3] row for matrix-matrix multiplication")
 
     def test_x_rotation_matrix(self):
         """ Test x_rotation_matrix """
@@ -70,3 +91,54 @@ class TestMat4x4(unittest.TestCase):
         self.assertEqual(row_1, z_rotation_matrix[1], "Asserting mat[1] row for z_rotation_matrix")
         self.assertEqual(row_2, z_rotation_matrix[2], "Asserting mat[2] row for z_rotation_matrix")
         self.assertEqual(row_3, z_rotation_matrix[3], "Asserting mat[3] row for z_rotation_matrix")
+
+    def test_translation_matrix(self):
+        """ Test translation_matrix """
+
+        translation_matrix = Mat4x4.translation_matrix(2.0, 3.0, 1.0).m
+
+        row_0 = [1.0, 0.0, 0.0, 0.0]
+        row_1 = [0.0, 1.0, 0.0, 0.0]
+        row_2 = [0.0, 0.0, 1.0, 0.0]
+        row_3 = [2.0, 3.0, 1.0, 1.0]
+
+        self.assertEqual(row_0, translation_matrix[0], "Asserting mat[0] row for translation_matrix")
+        self.assertEqual(row_1, translation_matrix[1], "Asserting mat[1] row for translation_matrix")
+        self.assertEqual(row_2, translation_matrix[2], "Asserting mat[2] row for translation_matrix")
+        self.assertEqual(row_3, translation_matrix[3], "Asserting mat[3] row for translation_matrix")
+
+    def test_multiply_matrix(self):
+        """ Testing multiply_matrix """
+        matrix_1 = Mat4x4.identity_matrix()
+        matrix_2 = Mat4x4()
+
+        matrix_2.m[0] = [4, 3, 1, 0]
+        matrix_2.m[2] = [2, 2, 2, 0]
+
+        result = Mat4x4.multiply_matrix(matrix_1, matrix_2)
+        result = result.m
+
+        row_0 = [4.0, 0.0, 2.0, 0.0]
+        row_1 = [3.0, 0.0, 2.0, 0.0]
+        row_2 = [1.0, 0.0, 2.0, 0.0]
+        row_3 = [0.0, 0.0, 0.0, 0.0]
+
+        self.assertEqual(row_0, result[0], "Asserting mat[0] row for matrix-matrix multiplication")
+        self.assertEqual(row_1, result[1], "Asserting mat[1] row for matrix-matrix multiplication")
+        self.assertEqual(row_2, result[2], "Asserting mat[2] row for matrix-matrix multiplication")
+        self.assertEqual(row_3, result[3], "Asserting mat[3] row for matrix-matrix multiplication")
+
+    def test_identity_matrix(self):
+        """ Test identity_matrix """
+
+        identity_matrix = Mat4x4.identity_matrix().m
+
+        row_0 = [1.0, 0.0, 0.0, 0.0]
+        row_1 = [0.0, 1.0, 0.0, 0.0]
+        row_2 = [0.0, 0.0, 1.0, 0.0]
+        row_3 = [0.0, 0.0, 0.0, 1.0]
+
+        self.assertEqual(row_0, identity_matrix[0], "Asserting mat[0] row for identity_matrix")
+        self.assertEqual(row_1, identity_matrix[1], "Asserting mat[1] row for identity_matrix")
+        self.assertEqual(row_2, identity_matrix[2], "Asserting mat[2] row for identity_matrix")
+        self.assertEqual(row_3, identity_matrix[3], "Asserting mat[3] row for identity_matrix")
